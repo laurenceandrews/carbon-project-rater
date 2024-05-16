eee3226c8df381feabf5e9db98305a61963189b8
Laurence Andrews <hello@laurenceandrews.com>
2024-05-16 15:35:53 +0100

    Add PostgreSQL integration and endpoint to fetch projects

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,30 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost/carbon_project_rater'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class CarbonProject(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text)
+    rating = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f'<CarbonProject {self.name}>'
 
 @app.route('/')
 def home():
     return "Hello, I'm a Carbon Project Rater!"
+
+@app.route('/projects')
+def get_projects():
+    projects = CarbonProject.query.all()
+    return { 'projects': [{ 'name': p.name, 'description': p.description, 'rating': p.rating } for p in projects] }
 
 if __name__ == '__main__':
     app.run(debug=True)
