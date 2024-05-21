9b5330d7b4bfdad60bd3a8b67e4cf82c651f8a72
laurenceandrews <hello@laurenceandrews.com>
2024-05-21 13:01:05 +0100

    Update Cypress frontend testing

--- frontend/cypress/integration/projects_spec.js
+++ frontend/cypress/integration/projects_spec.js
@@ -1,14 +1,12 @@
 describe('Projects Page', () => {
-    it('successfully loads and displays projects', () => {
-      // Arrange
-      cy.visit('http://localhost:3000/')
-  
-      // Act
-      cy.contains('Projects').click();
-  
-      // Assert
-      cy.url().should('include', '/projects')
-      cy.get('ul').find('li').should('have.length.at.least', 1)
-    });
+  it('successfully loads and displays projects', () => {
+    // Arrange
+    cy.visit('http://localhost:3000/')
+
+    // Act - Nothing to do yet
+
+    // Assert - Check that the main URL is loaded and projects are displayed
+    cy.url().should('eq', 'http://localhost:3000/')
+    cy.get('ul').find('li').should('have.length.at.least', 1)
   });
-  
+});
